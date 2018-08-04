7f2a2a31e49dcaa04ba459889049f4c3ff82308c
zsmb13 <braunmarci@gmail.com>
2018-08-04 19:29:39 +0200

    Updated port configs, added https redirect

--- static-site-frontend/src/main/kotlin/co/zsmb/staticsite/StaticSiteApplication.kt
+++ static-site-frontend/src/main/kotlin/co/zsmb/staticsite/StaticSiteApplication.kt
@@ -2,8 +2,16 @@ package co.zsmb.staticsite
 
 import co.zsmb.staticsite.data.ArticleRepository
 import co.zsmb.staticsite.data.CustomPageRepository
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,6 +23,36 @@ class StaticSiteApplication
 
 fun main(args: Array<String>) {
     runApplication<StaticSiteApplication>(*args)
+}
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat = object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context) {
+                val securityConstraint = SecurityConstraint()
+                securityConstraint.userConstraint = "CONFIDENTIAL"
+                val collection = SecurityCollection()
+                collection.addPattern("/*")
+                securityConstraint.addCollection(collection)
+                context.addConstraint(securityConstraint)
+            }
+        }
+        tomcat.addAdditionalTomcatConnectors(redirectConnector())
+        return tomcat
+    }
+
+    private fun redirectConnector(): Connector {
+        val connector = Connector("org.apache.coyote.http11.Http11NioProtocol")
+        connector.scheme = "http"
+        connector.port = 80
+        connector.secure = false
+        connector.redirectPort = 443
+        return connector
+    }
+
 }
 
 @Controller
